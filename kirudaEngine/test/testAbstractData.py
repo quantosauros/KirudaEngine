7a5fdb6413378ddbc537bc8791e3c303953b8b0a
quantosauros <jayjlee12@gmail.com>
2015-08-18 09:57:17 +0900

    clean

--- kirudaEngine/test/testAbstractData.py
+++ kirudaEngine/test/testAbstractData.py
@@ -3,25 +3,12 @@ Created on 2015. 8. 12.
 
 @author: Jay
 '''
-from engine.data.UnitData import UnitData
 from engine.selectAssets import SelectAsset
 from util.assetConditions import assetConditions
 from util.dataEnums import dataEnums
 from engine.data.AbstractData import AbstractData
-from util.QueryMaker import QueryMaker
 from util.Date import Date
-from util.SouthKoreaCalendar import SouthKoreaCalendar
-from util.BusinessDayConvention import BusinessDayConvention
 
-
-
-
-date1 = Date('20150815')
-date2 = Date.valueOf('19860111')
-
-calen = SouthKoreaCalendar.getCalendar(0)
-print calen.isHoliday(date1)
-print calen.adjustDate(date1, BusinessDayConvention.MODIFIED_FOLLOWING)
 
 periods = [Date('20150611'), Date('20150612'), Date('20150617'),]
 #Assets
@@ -51,18 +38,4 @@ for x in result :
 print result[0][0][1]
 print result[0][0][1][1]
 
-#===============================================================================
-# #Assets
-# sa = SelectAsset()
-# variables = [assetConditions.MARKET]
-# conditions = ["='KQ'"]
-# #variables = [assetConditions.CODE]
-# #conditions = [" in ('KS005930', 'KS008770')"]
-# #variables = [assetConditions.MARKET, assetConditions.PER]
-# #conditions = ["='KQ'", ">'9'"]
-# assets = sa.select(variables, conditions)
-# 
-# date = "20150601"
-# unitdata = UnitData(dat, date, assets)
-# unitdata.getResult()
-#===============================================================================
+
